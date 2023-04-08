67822d7dcde2ba028a703826e121cb8431fa480d
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-04-08 16:11:37 -0400

    Correct oneToMany entities to collections

--- src/main/java/io/eddb/eddb2backend/infrastructure/persistence/postgresql/entity/system/PostgresSecurityEntity.java
+++ src/main/java/io/eddb/eddb2backend/infrastructure/persistence/postgresql/entity/system/PostgresSecurityEntity.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
+
 @Entity
 @Data
 @Builder
@@ -20,7 +22,7 @@ public class PostgresSecurityEntity {
     private String name;
     
     @OneToMany
-    private PostgresSystemEntity postgresSystemEntity;
+    private Collection<PostgresSystemEntity> postgresSystemEntities;
     
     public void setId(Long id) {
         this.id = id;
