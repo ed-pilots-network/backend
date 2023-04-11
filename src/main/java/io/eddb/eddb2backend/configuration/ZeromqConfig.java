b6688806fd96dd43f562403fd8f28b24f6e101ac
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2023-04-11 12:52:53 +0200

    fixing import error

--- src/main/java/io/eddb/eddb2backend/configuration/ZeromqConfig.java
+++ src/main/java/io/eddb/eddb2backend/configuration/ZeromqConfig.java
@@ -1,20 +1,13 @@
 package io.eddb.eddb2backend.configuration;
 
 import io.eddb.eddb2backend.infrastructure.eddn.EddnMessageHandler;
-import io.eddb.eddb2backend.infrastructure.eddn.dto.CommodityDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.zeromq.channel.ZeroMqChannel;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.zeromq.ZContext;
 
 @Configuration
