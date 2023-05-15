be298d7488195b866049e559416b272218e81a63
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-05-15 11:35:06 -0400

    Fix CommodityEntityMapper and EconomyEntityMapper

--- message-processors/commodityv3-processor/src/main/java/io/edpn/backend/messageprocessor/commodityv3/infrastructure/persistence/mappers/CommodityEntityMapper.java
+++ message-processors/commodityv3-processor/src/main/java/io/edpn/backend/messageprocessor/commodityv3/infrastructure/persistence/mappers/CommodityEntityMapper.java
@@ -15,21 +15,11 @@ public interface CommodityEntityMapper {
             @Result(property = "id", column = "id", javaType = UUID.class, typeHandler = UuidTypeHandler.class),
             @Result(property = "name", column = "name")
     })
-    @Select("SELECT id, name FROM commodity")
-    List<CommodityEntity> findAll();
-
-    @ResultMap("CommodityEntityResult")
     @Select("SELECT id, name FROM commodity WHERE id = #{id}")
     Optional<CommodityEntity> findById(@Param("id") UUID id);
 
     @Insert("INSERT INTO commodity (id, name) VALUES (#{id}, #{name})")
     int insert(CommodityEntity commodityEntity);
-
-    @Update("UPDATE commodity SET name = #{name} WHERE id = #{id}")
-    int update(CommodityEntity commodityEntity);
-
-    @Delete("DELETE FROM commodity WHERE id = #{id}")
-    int delete(@Param("id") UUID id);
 
     @ResultMap("CommodityEntityResult")
     @Select("SELECT id, name FROM commodity WHERE name = #{name}")
