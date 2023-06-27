1d60e12815d2aae97c04c4afd39cb99ba8f14d22
Pieter Van Eeckhout <wn5987@vandevelde.eu>
2023-06-27 10:09:07 +0200

    #43 moving to yaml properties files, leveraging include in main application.yaml

--- trade-module/src/main/java/io/edpn/backend/trade/configuration/PropertyConfig.java
+++ trade-module/src/main/java/io/edpn/backend/trade/configuration/PropertyConfig.java
@@ -1,13 +1,7 @@
 package io.edpn.backend.trade.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @Configuration("TradeModulePropertyConfig")
-@PropertySources({
-        @PropertySource("classpath:trademodule.properties"),
-        @PropertySource("classpath:trademodule-local.properties"),
-})
 public class PropertyConfig {
 }
