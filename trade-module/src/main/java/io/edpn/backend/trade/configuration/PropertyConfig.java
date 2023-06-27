91b615952752a1fb9d47b0d5d71e8efb6ca0bcb5
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2023-06-28 00:07:36 +0200

    #43 - fixing application.properties files not overloading with multimodule setup
    * #43 - fixing application.properties files not overloading with multimodule setup
    
    * #43 - moving liquibase files
    
    * #43 moving to yaml properties files, leveraging include in main application.yaml
    
    * #39 making module names uniform

---
+++ trade-module/src/main/java/io/edpn/backend/trade/configuration/PropertyConfig.java
@@ -0,0 +1,7 @@
+package io.edpn.backend.trade.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("TradeModulePropertyConfig")
+public class PropertyConfig {
+}
