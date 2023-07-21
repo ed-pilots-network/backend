fa058c2dc6a18174ef80e2e6df050a7fa1b26525
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-07-21 13:48:13 -0400

    Add ValidatedCommodity and CommodityType to domain to map new tables

---
+++ trade-module/src/main/java/io/edpn/backend/trade/domain/model/ValidatedCommodity.java
@@ -0,0 +1,18 @@
+package io.edpn.backend.trade.domain.model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Builder
+public class ValidatedCommodity {
+    
+    private UUID id;
+    private String commodityName;
+    private String displayName;
+    private CommodityType type;
+}
