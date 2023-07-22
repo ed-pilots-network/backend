baee073f25e1dffa302da218db63c5860f14c1de
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-07-22 10:42:25 -0400

    Add endpoints for ValidatedCommodity/FindCommodity

--- trade-module/src/main/java/io/edpn/backend/trade/application/controller/v1/DefaultTradeModuleController.java
+++ trade-module/src/main/java/io/edpn/backend/trade/application/controller/v1/DefaultTradeModuleController.java
@@ -1,12 +1,14 @@
 package io.edpn.backend.trade.application.controller.v1;
 
-import io.edpn.backend.trade.application.dto.v1.CommodityMarketInfoResponse;
-import io.edpn.backend.trade.application.dto.v1.LocateCommodityRequest;
-import io.edpn.backend.trade.application.dto.v1.LocateCommodityResponse;
+import io.edpn.backend.trade.application.dto.v1.*;
 import io.edpn.backend.trade.domain.controller.v1.TradeModuleController;
+import io.edpn.backend.trade.domain.service.v1.FindCommodityService;
 import io.edpn.backend.trade.domain.service.v1.LocateCommodityService;
 import io.edpn.backend.trade.domain.service.v1.FindCommodityMarketInfoService;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 
 
@@ -15,6 +17,7 @@ public class DefaultTradeModuleController implements TradeModuleController {
 
     private final FindCommodityMarketInfoService findCommodityMarketInfoService;
     private final LocateCommodityService locateCommodityService;
+    private final FindCommodityService findCommodityService;
 
     @Override
     public List<CommodityMarketInfoResponse> getBestCommodityPrice() {
@@ -24,5 +27,20 @@ public class DefaultTradeModuleController implements TradeModuleController {
     @Override
     public List<LocateCommodityResponse> locateCommodityWithFilters(LocateCommodityRequest locateCommodityRequest) {
         return locateCommodityService.locateCommoditiesOrderByDistance(locateCommodityRequest);
+    }
+    
+    @Override
+    public Optional<FindCommodityResponse> findValidatedCommodityById(UUID id) {
+        return findCommodityService.findById(id);
+    }
+    
+    @Override
+    public List<FindCommodityResponse> findAllValidatedCommodities() {
+        return findCommodityService.findAll();
+    }
+    
+    @Override
+    public List<FindCommodityResponse> findValidatedCommodityByFilter(FindCommodityRequest findCommodityRequest) {
+        return findCommodityService.findByFilter(findCommodityRequest);
     }
 }
