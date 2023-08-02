0532dabeeb858c51829b528f98dfac76a626e669
pveechout <vaneeckhout.pieter@gmail.com>
2023-08-02 08:59:32 +0200

    #63 - moving transport layer annotations to adapter package

--- hex-exploration-module/src/main/java/io/edpn/backend/exploration/adapter/web/SystemController.java
+++ hex-exploration-module/src/main/java/io/edpn/backend/exploration/adapter/web/SystemController.java
@@ -5,17 +5,22 @@ import io.edpn.backend.exploration.application.port.incomming.FindSystemsFromSea
 import io.edpn.backend.exploration.application.port.incomming.SystemControllerPort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1/exploration/system")
 public class SystemController implements SystemControllerPort {
 
     final FindSystemsFromSearchbarUseCase findSystemsFromSearchbarUseCase;
 
     @Override
-    public List<SystemDto> findSystemsFromSearchBar(String subString, Integer amount) {
+    @GetMapping("/search-bar")
+    public List<SystemDto> findSystemsFromSearchBar(@RequestParam(name = "subString") String subString, @RequestParam(name = "amount", required = false, defaultValue = "10") Integer amount) {
         return findSystemsFromSearchbarUseCase.findSystemsFromSearchBar(subString, amount);
     }
 }
