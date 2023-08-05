6a3e1e1253c226841e084cdc0991d3140706b2e9
Pieter Van Eeckhout <pieter.vaneeckhout@hoakt.com>
2023-08-05 17:06:46 +0200

    #63 - remove unused mybatis mapper method

--- exploration-module/src/main/java/io/edpn/backend/exploration/adapter/persistence/MybatisSystemCoordinateRequestRepository.java
+++ exploration-module/src/main/java/io/edpn/backend/exploration/adapter/persistence/MybatisSystemCoordinateRequestRepository.java
@@ -4,12 +4,10 @@ import io.edpn.backend.exploration.adapter.persistence.entity.MybatisSystemCoord
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MybatisSystemCoordinateRequestRepository {
 
@@ -22,14 +20,10 @@ public interface MybatisSystemCoordinateRequestRepository {
     @Select("SELECT * FROM system_coordinate_data_request")
     List<MybatisSystemCoordinateRequestEntity> findAll();
 
-    @Select("SELECT * FROM system_coordinate_data_request WHERE requesting_module = #{requestingModule} AND system_name = #{systemName}")
+    @Select("SELECT * FROM system_coordinate_data_request WHERE system_name = #{systemName}")
     @Results(id = "commodityMarketInfoResultMap", value = {
             @Result(property = "systemName", column = "system_name"),
             @Result(property = "requestingModule", column = "requesting_module")
     })
-    Optional<MybatisSystemCoordinateRequestEntity> find(MybatisSystemCoordinateRequestEntity requestDataMessageEntity);
-
-    @Select("SELECT * FROM system_coordinate_data_request WHERE system_name = #{systemName}")
-    @ResultMap("commodityMarketInfoResultMap")
     List<MybatisSystemCoordinateRequestEntity> findBySystemName(String requestDataMessageEntity);
 }
