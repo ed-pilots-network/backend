0e5d87316060b9ed9e0fb2192c241786e092c851
Pieter Van Eeckhout <pieter.vaneeckhout@hoakt.com>
2023-08-06 00:06:23 +0200

    #63 - adding swagger documentation

--- exploration-module/src/main/java/io/edpn/backend/exploration/adapter/web/SystemController.java
+++ exploration-module/src/main/java/io/edpn/backend/exploration/adapter/web/SystemController.java
@@ -3,6 +3,8 @@ package io.edpn.backend.exploration.adapter.web;
 import io.edpn.backend.exploration.application.dto.SystemDto;
 import io.edpn.backend.exploration.application.port.incomming.FindSystemsByNameContainingUseCase;
 import io.edpn.backend.exploration.application.port.incomming.SystemControllerPort;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +22,11 @@ public class SystemController implements SystemControllerPort {
     final FindSystemsByNameContainingInputValidator findSystemsByNameContainingInputValidator;
 
     @Override
+    @Operation(summary = "Find systems by name containing a specific substring, will give priority to return systems starting with the given substring")
     @GetMapping("/by-name-containing")
-    public List<SystemDto> findByNameContaining(@RequestParam(name = "subString") String subString, @RequestParam(name = "amount", required = false, defaultValue = "10") Integer amount) {
+    public List<SystemDto> findByNameContaining(
+            @Parameter(description = "Substring to search for in the system name, minimal 3 characters", required = true) @RequestParam(name = "subString") String subString,
+            @Parameter(description = "Amount of results to retrieve. values 1...100",  required = false) @RequestParam(name = "amount", required = false, defaultValue = "10") Integer amount) {
         findSystemsByNameContainingInputValidator.validateSubString(subString);
         findSystemsByNameContainingInputValidator.validateAmount(amount);
         return findSystemsByNameContainingUseCase.findSystemsByNameContaining(subString, amount);
