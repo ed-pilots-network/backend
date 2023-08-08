fefcc8599c914ce94588585918cb4e6edfc174c7
pveechout <vaneeckhout.pieter@gmail.com>
2023-08-08 10:16:08 +0200

    adding missing processor beans

--- trade-module/src/main/java/io/edpn/backend/trade/configuration/MessagingConfig.java
+++ trade-module/src/main/java/io/edpn/backend/trade/configuration/MessagingConfig.java
@@ -2,9 +2,18 @@ package io.edpn.backend.trade.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.edpn.backend.messageprocessorlib.application.dto.eddn.CommodityMessage;
+import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.StationArrivalDistanceResponse;
+import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.StationMaxLandingPadSizeResponse;
+import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.SystemCoordinatesResponse;
+import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.SystemEliteIdResponse;
 import io.edpn.backend.messageprocessorlib.infrastructure.kafka.processor.MessageProcessor;
 import io.edpn.backend.trade.domain.usecase.ReceiveCommodityMessageUseCase;
+import io.edpn.backend.trade.domain.usecase.ReceiveDataRequestResponseUseCase;
 import io.edpn.backend.trade.infrastructure.kafka.processor.CommodityV3MessageProcessor;
+import io.edpn.backend.trade.infrastructure.kafka.processor.StationArrivalDistanceResponseMessageProcessor;
+import io.edpn.backend.trade.infrastructure.kafka.processor.StationMaxLandingPadSizeResponseMessageProcessor;
+import io.edpn.backend.trade.infrastructure.kafka.processor.SystemCoordinatesResponseMessageProcessor;
+import io.edpn.backend.trade.infrastructure.kafka.processor.SystemEliteIdResponseMessageProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +23,31 @@ public class MessagingConfig {
     @Bean(name = "tradeCommodityV3MessageProcessor")
     public MessageProcessor<CommodityMessage.V3> commodityV3MessageProcessor(ReceiveCommodityMessageUseCase receiveCommodityMessageUsecase, ObjectMapper objectMapper) {
         return new CommodityV3MessageProcessor(receiveCommodityMessageUsecase, objectMapper);
+    }
+
+    @Bean(name = "tradeSystemCoordinatesResponseMessageProcessor")
+    public MessageProcessor<SystemCoordinatesResponse> systemCoordinatesResponseMessageProcessor(ReceiveDataRequestResponseUseCase<SystemCoordinatesResponse> receiveSystemCoordinatesResponseUseCase, ObjectMapper objectMapper) {
+        return new SystemCoordinatesResponseMessageProcessor(receiveSystemCoordinatesResponseUseCase, objectMapper);
+    }
+
+    @Bean(name = "tradeSystemEliteIdResponseMessageProcessor")
+    public MessageProcessor<SystemEliteIdResponse> systemEliteIdResponseMessageProcessor(ReceiveDataRequestResponseUseCase<SystemEliteIdResponse> receiveSystemCoordinatesResponseUseCase, ObjectMapper objectMapper) {
+        return new SystemEliteIdResponseMessageProcessor(receiveSystemCoordinatesResponseUseCase, objectMapper);
+    }
+
+    @Bean(name = "tradeStationArrivalDistanceResponseMessageProcessor")
+    public MessageProcessor<StationArrivalDistanceResponse> stationArrivalDistanceResponseMessageProcessor(
+            ReceiveDataRequestResponseUseCase<StationArrivalDistanceResponse> receiveDataRequestResponseUseCase,
+            ObjectMapper objectMapper
+    ) {
+        return new StationArrivalDistanceResponseMessageProcessor(receiveDataRequestResponseUseCase, objectMapper);
+    }
+
+    @Bean(name = "tradeStationMaxLandingPadSizeResponseMessageProcessor")
+    public MessageProcessor<StationMaxLandingPadSizeResponse> stationMaxLandingPadSizeResponseMessageProcessor(
+            ReceiveDataRequestResponseUseCase<StationMaxLandingPadSizeResponse> receiveDataRequestResponseUseCase,
+            ObjectMapper objectMapper
+    ) {
+        return new StationMaxLandingPadSizeResponseMessageProcessor(receiveDataRequestResponseUseCase, objectMapper);
     }
 }
--- trade-module/src/main/java/io/edpn/backend/trade/configuration/UseCaseConfig.java
+++ trade-module/src/main/java/io/edpn/backend/trade/configuration/UseCaseConfig.java
@@ -2,12 +2,16 @@ package io.edpn.backend.trade.configuration;
 
 import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.StationArrivalDistanceResponse;
 import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.StationMaxLandingPadSizeResponse;
+import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.SystemCoordinatesResponse;
+import io.edpn.backend.messageprocessorlib.application.dto.eddn.data.SystemEliteIdResponse;
 import io.edpn.backend.trade.application.usecase.DefaultFindCommodityMarketInfoUseCase;
 import io.edpn.backend.trade.application.usecase.DefaultFindCommodityUseCase;
 import io.edpn.backend.trade.application.usecase.DefaultLocateCommodityUseCase;
 import io.edpn.backend.trade.application.usecase.DefaultReceiveCommodityMessageUseCase;
 import io.edpn.backend.trade.application.usecase.ReceiveStationArrivalDistanceResponseUseCase;
 import io.edpn.backend.trade.application.usecase.ReceiveStationMaxLandingPadSizeResponseUseCase;
+import io.edpn.backend.trade.application.usecase.ReceiveSystemCoordinatesResponseUseCase;
+import io.edpn.backend.trade.application.usecase.ReceiveSystemEliteIdResponseUseCase;
 import io.edpn.backend.trade.domain.model.Station;
 import io.edpn.backend.trade.domain.model.System;
 import io.edpn.backend.trade.domain.repository.CommodityMarketInfoRepository;
@@ -31,32 +35,42 @@ import java.util.List;
 @Configuration("TradeModuleUseCaseConfig")
 public class UseCaseConfig {
 
-    @Bean(name = "findBestCommodityPriceUseCase")
+    @Bean(name = "tradeFindBestCommodityPriceUseCase")
     public FindCommodityMarketInfoUseCase findCommodityMarketInfoCase(CommodityMarketInfoRepository commodityMarketInfoRepository) {
         return new DefaultFindCommodityMarketInfoUseCase(commodityMarketInfoRepository);
     }
 
-    @Bean(name = "receiveCommodityMessageUseCase")
+    @Bean(name = "tradeReceiveCommodityMessageUseCase")
     public ReceiveCommodityMessageUseCase receiveCommodityMessageUseCase(CommodityRepository commodityRepository, SystemRepository systemRepository, StationRepository stationRepository, MarketDatumRepository marketDatumRepository, List<RequestDataService<Station>> stationRequestDataServices, List<RequestDataService<System>> systemRequestDataServices) {
         return new DefaultReceiveCommodityMessageUseCase(commodityRepository, systemRepository, stationRepository, marketDatumRepository, stationRequestDataServices, systemRequestDataServices);
     }
 
-    @Bean(name = "receiveStationArrivalDistanceResponseUseCase")
+    @Bean(name = "tradeReceiveSystemEliteIdResponseUseCase")
+    public ReceiveDataRequestResponseUseCase<SystemEliteIdResponse> receiveSystemEliteIdResponseUseCase(SystemRepository systemRepository) {
+        return new ReceiveSystemEliteIdResponseUseCase(systemRepository);
+    }
+
+    @Bean(name = "tradeReceiveSystemCoordinatesResponseUseCase")
+    public ReceiveDataRequestResponseUseCase<SystemCoordinatesResponse> receiveSystemCoordinatesResponseUseCase(SystemRepository systemRepository) {
+        return new ReceiveSystemCoordinatesResponseUseCase(systemRepository);
+    }
+
+    @Bean(name = "traderReceiveStationArrivalDistanceResponseUseCase")
     public ReceiveDataRequestResponseUseCase<StationArrivalDistanceResponse> receiveStationArrivalDistanceResponseUseCase(SystemRepository systemRepository, StationRepository stationRepository) {
         return new ReceiveStationArrivalDistanceResponseUseCase(systemRepository, stationRepository);
     }
 
-    @Bean(name = "receiveStationMaxLandingPadSizeResponseUseCase")
+    @Bean(name = "tradeReceiveStationMaxLandingPadSizeResponseUseCase")
     public ReceiveDataRequestResponseUseCase<StationMaxLandingPadSizeResponse> receiveStationMaxLandingPadSizeResponseUseCase(SystemRepository systemRepository, StationRepository stationRepository) {
         return new ReceiveStationMaxLandingPadSizeResponseUseCase(systemRepository, stationRepository);
     }
 
-    @Bean(name = "locateCommodityUseCase")
+    @Bean(name = "tradeLocateCommodityUseCase")
     public LocateCommodityUseCase locateCommodityUseCase(LocateCommodityRepository locateCommodityRepository) {
         return new DefaultLocateCommodityUseCase(locateCommodityRepository);
     }
     
-    @Bean(name = "findCommodityUseCase")
+    @Bean(name = "tradFindCommodityUseCase")
     public FindCommodityUseCase findCommodityUseCase(ValidatedCommodityRepository validatedCommodityRepository) {
         return new DefaultFindCommodityUseCase(validatedCommodityRepository);
     }
