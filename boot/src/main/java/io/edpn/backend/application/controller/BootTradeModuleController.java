32ac68c78641790de9f24c54f9dcf0ac70e65da8
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-09 10:57:57 -0400

    Update Boot controller for hex

--- boot/src/main/java/io/edpn/backend/application/controller/BootTradeModuleController.java
+++ boot/src/main/java/io/edpn/backend/application/controller/BootTradeModuleController.java
@@ -1,15 +1,17 @@
 package io.edpn.backend.application.controller;
 
-import io.edpn.backend.trade.application.controller.v1.DefaultTradeModuleController;
-import io.edpn.backend.trade.domain.service.v1.FindCommodityMarketInfoService;
-import io.edpn.backend.trade.domain.service.v1.FindCommodityService;
-import io.edpn.backend.trade.domain.service.v1.LocateCommodityService;
+import io.edpn.backend.trade.adapter.web.ValidatedCommodityController;
+import io.edpn.backend.trade.application.usecase.validatedcommodity.FindAllValidatedCommodityUseCase;
+import io.edpn.backend.trade.application.usecase.validatedcommodity.FindValidatedCommodityByFilterUseCase;
+import io.edpn.backend.trade.application.usecase.validatedcommodity.FindValidatedCommodityByNameUseCase;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class BootTradeModuleController extends DefaultTradeModuleController {
-    public BootTradeModuleController(FindCommodityMarketInfoService findCommodityMarketInfoService, LocateCommodityService locateCommodityService, FindCommodityService findCommodityService) {
-        super(findCommodityMarketInfoService, locateCommodityService, findCommodityService);
+public class BootTradeModuleController extends ValidatedCommodityController {
+    public BootTradeModuleController(FindAllValidatedCommodityUseCase findAllValidatedCommodityUseCase,
+                                     FindValidatedCommodityByNameUseCase findValidatedCommodityByNameUseCase,
+                                     FindValidatedCommodityByFilterUseCase findValidatedCommodityByFilterUseCase) {
+        super(findAllValidatedCommodityUseCase, findValidatedCommodityByNameUseCase, findValidatedCommodityByFilterUseCase);
     }
 }
