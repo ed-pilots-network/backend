4633268845c973a26322793b424fe8d1d4a832b7
Pieter Van Eeckhout <pieter.vaneeckhout@hoakt.com>
2023-08-09 20:30:50 +0200

    fixing over constraining

--- exploration-module/src/main/java/io/edpn/backend/exploration/adapter/web/SystemController.java
+++ exploration-module/src/main/java/io/edpn/backend/exploration/adapter/web/SystemController.java
@@ -2,7 +2,6 @@ package io.edpn.backend.exploration.adapter.web;
 
 import io.edpn.backend.exploration.application.dto.SystemDto;
 import io.edpn.backend.exploration.application.port.incomming.FindSystemsByNameContainingUseCase;
-import io.edpn.backend.exploration.application.port.incomming.SystemControllerPort;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
@@ -16,17 +15,16 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/exploration/system")
-public class SystemController implements SystemControllerPort {
+public class SystemController {
 
     final FindSystemsByNameContainingUseCase findSystemsByNameContainingUseCase;
     final FindSystemsByNameContainingInputValidator findSystemsByNameContainingInputValidator;
 
-    @Override
     @Operation(summary = "Find systems by name containing a specific substring, will give priority to return systems starting with the given substring")
     @GetMapping("/by-name-containing")
     public List<SystemDto> findByNameContaining(
             @Parameter(description = "Substring to search for in the system name, minimal 3 characters", required = true) @RequestParam(name = "subString") String subString,
-            @Parameter(description = "Amount of results to retrieve. values 1...100",  required = false) @RequestParam(name = "amount", required = false, defaultValue = "10") Integer amount) {
+            @Parameter(description = "Amount of results to retrieve. values 1...100", required = false) @RequestParam(name = "amount", required = false, defaultValue = "10") Integer amount) {
         findSystemsByNameContainingInputValidator.validateSubString(subString);
         findSystemsByNameContainingInputValidator.validateAmount(amount);
         return findSystemsByNameContainingUseCase.findSystemsByNameContaining(subString, amount);
