1093919e59b62542da4fc631ae94689c351e7ee6
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-14 17:37:37 -0400

    Commodity Market Info repository + mapper

---
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/CommodityMarketInfoRepository.java
@@ -0,0 +1,26 @@
+package io.edpn.backend.trade.adapter.persistence;
+
+import io.edpn.backend.trade.adapter.persistence.entity.MybatisCommodityMarketInfoEntity;
+import io.edpn.backend.trade.adapter.persistence.repository.MybatisCommodityMarketInfoRepository;
+import io.edpn.backend.trade.application.domain.CommodityMarketInfo;
+import io.edpn.backend.trade.application.dto.persistence.entity.mapper.CommodityMarketInfoEntityMapper;
+import io.edpn.backend.trade.application.port.outgoing.commoditymarketinfo.GetFullCommodityMarketInfoPort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CommodityMarketInfoRepository implements GetFullCommodityMarketInfoPort {
+    
+    private final MybatisCommodityMarketInfoRepository mybatisCommodityMarketInfoRepository;
+    private final CommodityMarketInfoEntityMapper<MybatisCommodityMarketInfoEntity> commodityMarketInfoEntityMapper;
+    
+    @Override
+    public List<CommodityMarketInfo> findAll() {
+        return mybatisCommodityMarketInfoRepository
+                .findAll()
+                .stream()
+                .map(commodityMarketInfoEntityMapper::map)
+                .toList();
+    }
+}
