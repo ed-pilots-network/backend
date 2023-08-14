80cf72248f091ace1bde6230ac8ecde46cd6d320
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-14 17:39:16 -0400

    Fill out FindCommodityMarketInfoService

--- hex-trade-module/src/main/java/io/edpn/backend/trade/application/service/FindCommodityMarketInfoService.java
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/application/service/FindCommodityMarketInfoService.java
@@ -1,14 +1,25 @@
 package io.edpn.backend.trade.application.service;
 
-import io.edpn.backend.trade.application.domain.CommodityMarketInfo;
+import io.edpn.backend.trade.application.dto.web.object.CommodityMarketInfoDto;
+import io.edpn.backend.trade.application.dto.web.object.mapper.CommodityMarketInfoDtoMapper;
 import io.edpn.backend.trade.application.port.incomming.commoditymarketinfo.GetFullCommodityMarketInfoUseCase;
+import io.edpn.backend.trade.application.port.outgoing.commoditymarketinfo.GetFullCommodityMarketInfoPort;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-//TODO: create mappings for Web
+@RequiredArgsConstructor
 public class FindCommodityMarketInfoService implements GetFullCommodityMarketInfoUseCase {
+    
+    private final GetFullCommodityMarketInfoPort commodityMarketInfoPort;
+    private final CommodityMarketInfoDtoMapper commodityMarketInfoDtoMapper;
+    
     @Override
-    public List<CommodityMarketInfo> findAll() {
-        return null;
+    public List<CommodityMarketInfoDto> findAll() {
+        return commodityMarketInfoPort
+                .findAll()
+                .stream()
+                .map(commodityMarketInfoDtoMapper::map)
+                .toList();
     }
 }
