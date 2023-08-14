d5debdd321f9ba1ec474b2bcf4c834aa9229178d
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-14 17:39:29 -0400

    Replace with Dto

--- hex-trade-module/src/main/java/io/edpn/backend/trade/application/port/incomming/commoditymarketinfo/GetFullCommodityMarketInfoUseCase.java
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/application/port/incomming/commoditymarketinfo/GetFullCommodityMarketInfoUseCase.java
@@ -1,10 +1,10 @@
 package io.edpn.backend.trade.application.port.incomming.commoditymarketinfo;
 
-import io.edpn.backend.trade.application.domain.CommodityMarketInfo;
+import io.edpn.backend.trade.application.dto.web.object.CommodityMarketInfoDto;
 
 import java.util.List;
 
 public interface GetFullCommodityMarketInfoUseCase {
     
-    List<CommodityMarketInfo> findAll();
+    List<CommodityMarketInfoDto> findAll();
 }
