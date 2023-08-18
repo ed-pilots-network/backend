b8d63b557aaa777d326ad9346668c0802cb475d3
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-18 17:08:18 -0400

    Commented out TODO

--- hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/repository/MybatisRequestDataMessageRepository.java
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/repository/MybatisRequestDataMessageRepository.java
@@ -1,25 +1,17 @@
 package io.edpn.backend.trade.adapter.persistence.repository;
 
-import io.edpn.backend.trade.infrastructure.persistence.entity.RequestDataMessageEntity;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
-import java.util.Optional;
-
 //TODO: KAFKA Messaging
 public interface MybatisRequestDataMessageRepository {
-
-    @Insert("INSERT INTO request_data_message (topic, message) VALUES (#{topic}, #{message})")
-    void insert(RequestDataMessageEntity requestDataMessageEntity);
-
-    @Delete("DELETE FROM request_data_message WHERE topic = #{topic} AND message = #{message}")
-    void delete(RequestDataMessageEntity requestDataMessageEntity);
-
-    @Select("SELECT * FROM request_data_message")
-    List<RequestDataMessageEntity> findAll();
-
-    @Select("SELECT * FROM request_data_message WHERE topic = #{topic} AND message = #{message}")
-    Optional<RequestDataMessageEntity> find(RequestDataMessageEntity requestDataMessageEntity);
+//
+//    @Insert("INSERT INTO request_data_message (topic, message) VALUES (#{topic}, #{message})")
+//    void insert(RequestDataMessageEntity requestDataMessageEntity);
+//
+//    @Delete("DELETE FROM request_data_message WHERE topic = #{topic} AND message = #{message}")
+//    void delete(RequestDataMessageEntity requestDataMessageEntity);
+//
+//    @Select("SELECT * FROM request_data_message")
+//    List<RequestDataMessageEntity> findAll();
+//
+//    @Select("SELECT * FROM request_data_message WHERE topic = #{topic} AND message = #{message}")
+//    Optional<RequestDataMessageEntity> find(RequestDataMessageEntity requestDataMessageEntity);
 }
