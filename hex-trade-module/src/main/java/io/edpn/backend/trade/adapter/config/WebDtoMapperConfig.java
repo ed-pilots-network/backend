31c7235110030f0d4a283c8b465c8e8a3043c871
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-18 17:08:44 -0400

    Adding mappers to config

--- hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/config/WebDtoMapperConfig.java
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/config/WebDtoMapperConfig.java
@@ -1,8 +1,12 @@
 package io.edpn.backend.trade.adapter.config;
 
 import io.edpn.backend.trade.adapter.web.dto.filter.mapper.RestFindCommodityFilterDtoMapper;
+import io.edpn.backend.trade.adapter.web.dto.object.mapper.RestStationDtoMapper;
+import io.edpn.backend.trade.adapter.web.dto.object.mapper.RestSystemDtoMapper;
 import io.edpn.backend.trade.adapter.web.dto.object.mapper.RestValidatedCommodityDtoMapper;
 import io.edpn.backend.trade.application.dto.web.filter.mapper.FindCommodityFilterDtoMapper;
+import io.edpn.backend.trade.application.dto.web.object.mapper.StationDtoMapper;
+import io.edpn.backend.trade.application.dto.web.object.mapper.SystemDtoMapper;
 import io.edpn.backend.trade.application.dto.web.object.mapper.ValidatedCommodityDtoMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +22,16 @@ public class WebDtoMapperConfig {
     @Bean(name = "tradeValidatedCommodityDTOMapper")
     public ValidatedCommodityDtoMapper validatedCommodityDTOMapper() {
         return new RestValidatedCommodityDtoMapper();
+    }
+    
+    @Bean(name = "tradeSystemDtoMapper")
+    public SystemDtoMapper systemDtoMapper() {
+        return new RestSystemDtoMapper();
+    }
+    
+    @Bean(name = "tradeStationDtoMapper")
+    public StationDtoMapper stationDtoMapper(
+            SystemDtoMapper systemDtoMapper){
+        return new RestStationDtoMapper(systemDtoMapper);
     }
 }
