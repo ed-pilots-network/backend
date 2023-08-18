8926db8f2674676ae683880f83537e7946045033
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-18 12:32:45 -0400

    Corrected mappings to use full objects

--- hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/web/dto/object/mapper/RestLocateCommodityDtoMapper.java
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/adapter/web/dto/object/mapper/RestLocateCommodityDtoMapper.java
@@ -5,20 +5,21 @@ import io.edpn.backend.trade.application.domain.LocateCommodity;
 import io.edpn.backend.trade.application.dto.web.object.LocateCommodityDto;
 import io.edpn.backend.trade.application.dto.web.object.mapper.LocateCommodityDtoMapper;
 import io.edpn.backend.trade.application.dto.web.object.mapper.StationDtoMapper;
+import io.edpn.backend.trade.application.dto.web.object.mapper.ValidatedCommodityDtoMapper;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class RestLocateCommodityDtoMapper implements LocateCommodityDtoMapper {
     
     private final StationDtoMapper stationDtoMapper;
+    private final ValidatedCommodityDtoMapper commodityDtoMapper;
     
     @Override
     public LocateCommodityDto map(LocateCommodity locateCommodity) {
         return new RestLocateCommodityDto(
-                locateCommodity.validatedCommodity().displayName(),
+                commodityDtoMapper.map(locateCommodity.validatedCommodity()),
                 stationDtoMapper.map(locateCommodity.station()),
-                locateCommodity.system().name(),
-                locateCommodity.pricesUpdatedAt(),
+                locateCommodity.priceUpdatedAt(),
                 locateCommodity.supply(),
                 locateCommodity.demand(),
                 locateCommodity.buyPrice(),
