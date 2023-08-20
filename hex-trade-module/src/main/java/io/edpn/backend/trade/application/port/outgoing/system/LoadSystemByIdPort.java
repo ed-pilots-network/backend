311e17ce216f3132bb951758e7eda8304eb54677
Daniel-J-Mason <daniel.j.msn@gmail.com>
2023-08-20 13:00:55 -0400

    Convert to optionals

--- hex-trade-module/src/main/java/io/edpn/backend/trade/application/port/outgoing/system/LoadSystemByIdPort.java
+++ hex-trade-module/src/main/java/io/edpn/backend/trade/application/port/outgoing/system/LoadSystemByIdPort.java
@@ -1,8 +1,11 @@
 package io.edpn.backend.trade.application.port.outgoing.system;
 
+import io.edpn.backend.trade.application.domain.System;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LoadSystemByIdPort {
     
-    System loadById(UUID uuid);
+    Optional<System> loadById(UUID uuid);
 }
