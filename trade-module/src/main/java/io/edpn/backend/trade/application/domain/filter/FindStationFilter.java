f527e8f97af6350a00d441a7f6f204bef9e07bd8
pveeckhout <vaneeckhout.pieter@gmail.com>
2023-09-06 17:11:35 +0200

    #108 adding periodic station data requests

---
+++ trade-module/src/main/java/io/edpn/backend/trade/application/domain/filter/FindStationFilter.java
@@ -0,0 +1,18 @@
+package io.edpn.backend.trade.application.domain.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class FindStationFilter {
+    private Boolean hasRequiredOdyssey;
+}
