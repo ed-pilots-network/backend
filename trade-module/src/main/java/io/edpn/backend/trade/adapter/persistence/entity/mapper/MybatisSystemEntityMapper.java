fb9a5b6ed8e3f2e1c8d3c86d0c15735403e8b7b9
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2023-11-11 20:35:47 +0100

    #128 - fixing npe for coordinate mapping

--- trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/entity/mapper/MybatisSystemEntityMapper.java
+++ trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/entity/mapper/MybatisSystemEntityMapper.java
@@ -6,6 +6,8 @@ import io.edpn.backend.trade.application.domain.System;
 import io.edpn.backend.trade.application.dto.persistence.entity.SystemEntity;
 import io.edpn.backend.trade.application.dto.persistence.entity.mapper.SystemEntityMapper;
 
+import java.util.Optional;
+
 public class MybatisSystemEntityMapper implements SystemEntityMapper<MybatisSystemEntity> {
     @Override
     public System map(SystemEntity systemEntity) {
@@ -20,16 +22,16 @@ public class MybatisSystemEntityMapper implements SystemEntityMapper<MybatisSyst
                 )
         );
     }
-    
+
     @Override
     public MybatisSystemEntity map(System system) {
         return MybatisSystemEntity.builder()
                 .id(system.id())
                 .eliteId(system.eliteId())
                 .name(system.name())
-                .xCoordinate(system.coordinate().x())
-                .yCoordinate(system.coordinate().y())
-                .zCoordinate(system.coordinate().z())
+                .xCoordinate(Optional.ofNullable(system.coordinate()).map(Coordinate::x).orElse(null))
+                .yCoordinate(Optional.ofNullable(system.coordinate()).map(Coordinate::y).orElse(null))
+                .zCoordinate(Optional.ofNullable(system.coordinate()).map(Coordinate::z).orElse(null))
                 .build();
     }
 }
--- trade-module/src/test/java/io/edpn/backend/trade/adapter/persistence/entity/mapper/MybatisSystemEntityMapperTest.java
+++ trade-module/src/test/java/io/edpn/backend/trade/adapter/persistence/entity/mapper/MybatisSystemEntityMapperTest.java
@@ -4,26 +4,28 @@ import io.edpn.backend.trade.adapter.persistence.entity.MybatisSystemEntity;
 import io.edpn.backend.trade.application.domain.Coordinate;
 import io.edpn.backend.trade.application.domain.System;
 import io.edpn.backend.trade.application.dto.persistence.entity.mapper.SystemEntityMapper;
-import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 @ExtendWith(MockitoExtension.class)
 class MybatisSystemEntityMapperTest {
-    
+
     private SystemEntityMapper<MybatisSystemEntity> underTest;
-    
+
     @BeforeEach
     public void setUp() {
         underTest = new MybatisSystemEntityMapper();
     }
-    
+
     @Test
     public void testMap_givenEntity_shouldReturnDomainObject() {
         UUID id = UUID.randomUUID();
@@ -40,7 +42,7 @@ class MybatisSystemEntityMapperTest {
                 .yCoordinate(yCoordinate)
                 .zCoordinate(zCoordinate)
                 .build();
-        
+
         System domainObject = underTest.map(entity);
 
         assertThat(domainObject.id(), is(id));
@@ -51,7 +53,7 @@ class MybatisSystemEntityMapperTest {
         assertThat(domainObject.coordinate().y(), is(yCoordinate));
         assertThat(domainObject.coordinate().z(), is(zCoordinate));
     }
-    
+
     @Test
     public void testMap_givenDomainObject_shouldReturnEntity() {
         UUID id = UUID.randomUUID();
@@ -63,14 +65,32 @@ class MybatisSystemEntityMapperTest {
         Coordinate coordinate = new Coordinate(xCoordinate, yCoordinate, zCoordinate);
         System domainObject = new System(id, eliteId, name, coordinate);
 
-        
+
         MybatisSystemEntity entity = underTest.map(domainObject);
-        
+
         assertThat(entity.getId(), is(id));
         assertThat(entity.getEliteId(), is(eliteId));
         assertThat(entity.getName(), is(name));
         assertThat(entity.getXCoordinate(), is(xCoordinate));
         assertThat(entity.getYCoordinate(), is(yCoordinate));
         assertThat(entity.getZCoordinate(), is(zCoordinate));
+    }
+
+    @Test
+    public void testMap_givenDomainObject_shouldReturnEntity_nullCoordinate() {
+        UUID id = UUID.randomUUID();
+        Long eliteId = 123456L;
+        String name = "System Name";
+        System domainObject = new System(id, eliteId, name, null);
+
+
+        MybatisSystemEntity entity = underTest.map(domainObject);
+
+        assertThat(entity.getId(), is(id));
+        assertThat(entity.getEliteId(), is(eliteId));
+        assertThat(entity.getName(), is(name));
+        assertThat(entity.getXCoordinate(), nullValue());
+        assertThat(entity.getYCoordinate(), nullValue());
+        assertThat(entity.getZCoordinate(), nullValue());
     }
 }
