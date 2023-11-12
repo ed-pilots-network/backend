7d7f4cec22e94e9c0f76359171f29ddbf27543ff
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2023-11-12 21:58:42 +0100

    #128 - fixing default maxLandingPadSize field value in Station record

--- trade-module/src/main/java/io/edpn/backend/trade/application/domain/Station.java
+++ trade-module/src/main/java/io/edpn/backend/trade/application/domain/Station.java
@@ -3,6 +3,7 @@ package io.edpn.backend.trade.application.domain;
 import lombok.With;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,6 +24,9 @@ public record Station(
     public Station {
         if (maxLandingPadSize == null) {
             maxLandingPadSize = LandingPadSize.UNKNOWN;
+        }
+        if (marketData == null) {
+            marketData = new ArrayList<>();
         }
     }
 }
