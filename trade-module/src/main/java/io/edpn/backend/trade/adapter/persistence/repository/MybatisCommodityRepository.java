61775480b9a87c0451dc8665358eae8d03869e4b
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2024-03-25 01:42:06 +0100

    Refactor commodity repository and update database schema (#158)
    * Refactor commodity repository and update database schema
    
    Updated MybatisCommodityRepository, replacing several methods with a createOrUpdateOnConflict method and added new pertinent fields to the database table "commodity". This includes adding and renaming columns, updating the "validated_commodity_view" and adding an index for the "is_rare" column. In "build.gradle", the backendMessageProcessorLibVersion has been updated.
    
    * Add null values in ReceiveCommodityMessageUseCaseTest
    
    In the 'ReceiveCommodityMessageUseCaseTest.java', two new null parameters were added to the 'CommodityMessage.V3.Payload()' invocation. This aligns with recent schema updates to the Commodity Message functionality.
    
    * #157 Update validated_commodity_view settings
    
    The settings of the validated_commodity_view have been updated to run on change, in addition to running always. The 'create_validated_commodity_view' changeset in 'create_validated_commodity_view.xml' now includes 'runOnChange' making the database changes more responsive to modifications.

--- trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/repository/MybatisCommodityRepository.java
+++ trade-module/src/main/java/io/edpn/backend/trade/adapter/persistence/repository/MybatisCommodityRepository.java
@@ -1,45 +1,30 @@
 package io.edpn.backend.trade.adapter.persistence.repository;
 
-import io.edpn.backend.mybatisutil.UuidTypeHandler;
 import io.edpn.backend.trade.adapter.persistence.entity.MybatisCommodityEntity;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 public interface MybatisCommodityRepository {
 
-    @Select("SELECT * FROM commodity")
-    @Results(id = "commodityResultMap", value = {
-            @Result(property = "id", column = "id", javaType = UUID.class, typeHandler = UuidTypeHandler.class),
-            @Result(property = "name", column = "name", javaType = String.class)
-    })
-    List<MybatisCommodityEntity> findAll();
-
-    @Select("SELECT * FROM commodity WHERE id = #{id}")
-    @ResultMap("commodityResultMap")
-    Optional<MybatisCommodityEntity> findById(@Param("id") UUID id);
-
-    @Select("SELECT * FROM commodity WHERE name = #{name}")
-    @ResultMap("commodityResultMap")
-    Optional<MybatisCommodityEntity> findByName(@Param("name") String name);
-
-    @Insert("INSERT INTO commodity (id, name) VALUES (#{id}, #{name})")
-    void insert(MybatisCommodityEntity commodity);
-
-    @Select({
-            "INSERT INTO commodity (id, name)",
-            "VALUES (#{id}, #{name})",
-            "ON CONFLICT (name)",
-            "DO UPDATE SET",
-            "name = COALESCE(commodity.name, EXCLUDED.name)",
-            "RETURNING *"
-    })
+    @Select("""
+            WITH enriched_data AS (
+                SELECT v.id AS id, v.name AS technical_name, cw.display_name, cw.type, cw.is_rare
+                FROM (VALUES (#{id}, #{name})) AS v(id, name)
+                    LEFT JOIN commodity_whitelist cw
+                        ON v.name = cw.technical_name)
+            INSERT
+            INTO commodity (id, technical_name, display_name, type, is_rare)
+            SELECT id,
+                   technical_name,
+                   display_name,
+                   type,
+                   is_rare
+            FROM enriched_data
+            ON CONFLICT (technical_name)
+                DO UPDATE SET technical_name = COALESCE(commodity.technical_name, EXCLUDED.technical_name),
+                              display_name   = EXCLUDED.display_name,
+                              type           = EXCLUDED.type,
+                              is_rare        = EXCLUDED.is_rare
+            RETURNING *;
+            """)
     MybatisCommodityEntity createOrUpdateOnConflict(MybatisCommodityEntity commodity);
 }
